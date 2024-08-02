c2dd84b8ef34800ca0db952b487c0349a5dc387e
Muzammil <muzammilrazaofficial@gmail.com>
2024-08-03 00:26:15 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// content > quizs > quiz_result
+
+function playQuiz(){
+
+    document.getElementById("content").style.display = "none";
+    document.getElementById("quizs").style.display = "block";
+
+
+
+}
